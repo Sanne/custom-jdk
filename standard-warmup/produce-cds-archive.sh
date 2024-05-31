99a9eebf960f0c073d06ab2993a22fca0955dafd
Sanne Grinovero <sanne@hibernate.org>
2024-05-31 16:25:10 +0100

    Enable also -XX:+ArchiveInvokeDynamic during CDS archive generation

--- standard-warmup/produce-cds-archive.sh
+++ standard-warmup/produce-cds-archive.sh
@@ -3,4 +3,4 @@
 mvn package -DskipTests
 curl --retry-connrefused --retry 100 --retry-delay 1 -s -o /dev/null "http://localhost:8080/hello/quit" & 
 #java -jar target/quarkus-app/quarkus-run.jar
-/leyden-openjdk/bin/java -Xlog:cds -XX:CacheDataStore=/leyden-openjdk/quarkus-generic-cds.cds -jar target/quarkus-app/quarkus-run.jar
+/leyden-openjdk/bin/java -Xlog:cds -XX:+ArchiveInvokeDynamic -XX:CacheDataStore=/leyden-openjdk/quarkus-generic-cds.cds -jar target/quarkus-app/quarkus-run.jar
