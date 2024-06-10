a2d4736d4ccdc41d22a276792040848dea77a3ee
Sanne Grinovero <sanne@hibernate.org>
2024-06-10 14:16:55 +0100

    Drop VM caches before each measurement round

--- measure-boot.sh
+++ measure-boot.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 
 forceClean() {
-    podman rm -f measurement > /dev/null
+    podman rm -f $1 > /dev/null
     rm --force startedTimestamp
 }
 
+preMeasure() {
+    sync && sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+}
+
 measure() {
-    CONTAINER=$1
-    forceClean
+    SHORT_CONTAINERNAME=$1
+    FULLCONTAINERNAME="localhost/$SHORT_CONTAINERNAME:latest"
+    TEMP_INTANCE_NAME="measure_$SHORT_CONTAINERNAME"
+    forceClean $TEMP_INTANCE_NAME
     
     while [[ "$(curl -s -o startedTimestamp -w ''%{http_code}'' localhost:8080/currenttime)" != "200" ]]; do sleep .00001; done &
     
+    preMeasure
     startTimeMilliseconds=$(($(date --utc +%s%N)/1000000))
     
     # Options such as cpuset-mems=0 might require some extra system settings; see: https://fossies.org/linux/podman/troubleshooting.md
-    podman run --pull=never --read-only --rm -d -p 8080:8080 --cpus 4 --cpuset-mems=0 --memory 2000m --name measurement $CONTAINER > /dev/null
+    #TODO pick specific CPUs
+    podman run --pull=never --read-only --rm -d -p 8080:8080 --cpus 4 --cpuset-mems=0 --memory 2000m --name $TEMP_INTANCE_NAME $FULLCONTAINERNAME > /dev/null
 
     # For experiments:
     # podman run --pull=never --read-only --rm -it -p 8080:8080 --cpus 4 --cpuset-mems=0 --memory 2000m --name measurement localhost/leyden-build:latest
@@ -23,16 +31,17 @@ measure() {
     do
         sleep 1
     done
+    #TODO get memory data sleep 100
     
     finishTString=$(cat startedTimestamp)
-    forceClean
+    forceClean $TEMP_INTANCE_NAME
     #echo "Just before start timestamp: $startTimeMilliseconds"
     #echo "Bootstrap complete timestamp: $finishTString"
     
     deltaMilliseconds=$((finishTString - startTimeMilliseconds))
-    echo "Container $CONTAINER completed bootstrap in $deltaMilliseconds milliseconds"
+    echo "Container '$SHORT_CONTAINERNAME' completed bootstrap in $deltaMilliseconds milliseconds"
 }
 
-measure "localhost/leyden-build:latest"
-measure "localhost/baseline-boot:latest"
-measure "localhost/latestjdk-build:latest"
+measure "leyden-build"
+measure "baseline-boot"
+measure "latestjdk-build"
